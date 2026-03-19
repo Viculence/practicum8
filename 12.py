855174d0d5ae42239c3fded4a9e047cec36e8825
Viktoriya Yasminskaya <viculence@mail.ru>
2026-03-19 10:03:58 +0700

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,8 +1,7 @@
 import keyword
 
 def valid_variable_name(name):
-    if name.isidentifier() and not name[0].isdigit() and \
-       name not in keyword.kwlist:
+    if name.isidentifier() and name not in keyword.kwlist:
         return "Допустимое имя переменной"
     else:
         return "Недопустимое имя переменной"
